e58950df28ed99bbbc6f4dde2542ce1a43de0541
Victorine Herbin <herbin24u@etu.univ-lorraine.fr>
2023-09-18 15:08:10 +0200

    ajout de scripts sans exécutables

---
+++ script_Rno_tests.bat
@@ -0,0 +1,6 @@
+cd O:\ENSG\Semestre_9\Bonn\HelloProj
+mkdir build_script
+cd build_script
+cmake -DCMAKE_BUILD_TYPE=Release -DHELLOWORLD_WITH_TESTS="OFF" -DHELLOWORLD_WITH_EXECUTABLES="ON" ..
+cmake --build .
+ctest -C Release
